fe24bc5e94f857b19c95f6da1ca6d25a860aa693
David <wasd.rrre64@gmail.com>
2021-10-08 15:41:52 +0300

    ver.2.0

---
+++ app/routes.py
@@ -0,0 +1,23 @@
+from flask import render_template 
+from app import app 
+ 
+Василий='username'
+@app.route('/') 
+@app.route('/index') 
+def index(): 
+    user = {'username': 'Учитель'} 
+    posts = [ 
+        { 
+            'author': {'username': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        }, 
+        { 
+            'author': {'username': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        },  
+        { 
+            'author': {'username': "Давид"}, 
+            'body':'вы самый лучший преподаватель в МТУСИ' 
+        } 
+    ]  
+    return render_template('index.html', title='Home', user=user, posts=posts) 
